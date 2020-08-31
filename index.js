b89179d8d57e557dafe3a8f3fbe9423ff72d677c
nikitatonkoshkur <tonkoshkur@oneplanetops.com>
2020-08-31 23:16:08 +0300

    Fixes to index.js. Added support for new format

--- index.js
+++ index.js
@@ -316,24 +316,14 @@ var handleCatchAll = function(event, context) {
     var record = event.Records[0]
     var subject = record.Sns.Subject
     var timestamp = new Date(record.Sns.Timestamp).getTime() / 1000;
-    var message = JSON.parse(record.Sns.Message)
+    var message = record.Sns.Message
+    var messageType = record.Sns.Type
     var color = "warning";
 
-    if (message.NewStateValue === "ALARM") {
+    if (messageType === "ALARM") {
         color = "danger";
-    } else if (message.NewStateValue === "OK") {
+    } else if (messageType === "OK") {
         color = "good";
-    }
-
-    // Add all of the values from the event message to the Slack message description
-    var description = ""
-    for(key in message) {
-
-        var renderedMessage = typeof message[key] === 'object'
-                            ? JSON.stringify(message[key])
-                            : message[key]
-
-        description = description + "\n" + key + ": " + renderedMessage
     }
 
     var slackMessage = {
@@ -343,7 +333,7 @@ var handleCatchAll = function(event, context) {
             "color": color,
             "fields": [
               { "title": "Message", "value": record.Sns.Subject, "short": false },
-              { "title": "Description", "value": description, "short": false }
+              { "title": "Description", "value": message, "short": false }
             ],
             "ts": timestamp
           }
